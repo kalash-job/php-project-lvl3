fadf9dbb7984abe01d195471a1c35c4800f6815e
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-12-01 22:32:19 +0300

    To correct test of DomainCheckController

--- tests/Feature/DomainCheckControllerTest.php
+++ tests/Feature/DomainCheckControllerTest.php
@@ -25,12 +25,12 @@ class DomainCheckControllerTest extends TestCase
             'status_code' => 200,
             'h1' => 'Rukodeling.ru',
             'keywords' => 'Квиллинг, Скарпбукинг, товары для хобби, флористика, бумага для квиллинга, инструменты',
-            'description' => 'Сайт Интернет-магазина "Рукоделинг.ru" - товары для квиллинга, скрапбукинга и флористики по лучшим ценам.'
+            'description' => 'Товары для квиллинга, скрапбукинга и флористики по лучшим ценам.'
         ];
-        $responseBody = file_get_contents('tests/fixtures/response.txt');
+        $responseBody = file_get_contents('tests/fixtures/response.html');
 
         Http::fake([
-            'rukodeling.ru/*' => Http::response(['body' => $responseBody], 200)
+            '*' => Http::response($responseBody, 200)
         ]);
 
         $response = $this->post(route('domains.checks.store', ['id' => 1]));
