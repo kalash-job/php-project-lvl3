a05f4533b240d9bbf9d3196308451c64aff8b9a8
Nikolay Kalashnikov <kalash-job@yandex.ru>
2020-12-17 05:47:48 +0300

    Rename HomeControllerTest.php to HomeTest.php

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class HomeControllerTest extends TestCase
+class HomeTest extends TestCase
 {
     public function setUp(): void
     {
